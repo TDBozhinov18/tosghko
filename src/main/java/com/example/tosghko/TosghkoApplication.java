be6aacbd375e4740a7f5bd8f632cc069febac311
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2022-11-07 13:54:29 +0200

    Initial commit

---
+++ src/main/java/com/example/tosghko/TosghkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tosghko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosghkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosghkoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tosghko/TosghkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tosghko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosghkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
